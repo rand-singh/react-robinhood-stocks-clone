7a37529d0431c879726c7a4bc1d0d6de87dfde6e
Randeep Singh <rand-singh@users.noreply.github.com>
2021-02-07 18:29:49 +0000

    Scaffold App
    Remove OOB files, install node sass and normalize, setup basic style folders, setup header and body components.

---
+++ src/components/Body.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Body() {
+    return (
+        <div className="body">
+            body component            
+        </div>
+    )
+}
+
+export default Body
---
+++ src/components/Header.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Header() {
+    return (
+        <div className="header">
+            header component
+            
+        </div>
+    )
+}
+
+export default Header
