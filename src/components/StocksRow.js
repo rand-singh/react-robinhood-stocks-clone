e449778ac04a6eb6e9fa86fc96cb74dc2fef89f9
Randeep Singh <rand-singh@users.noreply.github.com>
2021-02-10 20:08:58 +0000

    Calculate share price increase/decrease

--- src/components/StocksRow.js
+++ src/components/StocksRow.js
@@ -1,15 +1,17 @@
 import React from 'react'
 
-function StocksRow({ name, openPrice, price }) {    
+function StocksRow({ name, openPrice, price, shares }) {
+    const percentage = ((price - openPrice) / openPrice) * 100
+
     return (
         <div className="stock">
             <div className="stock__intro">
                 <p className="stock__name">{name}</p>
-                <p className="stock__count">200 Shares</p>
+                <p className="stock__count">{shares ? (shares + " Shares") : '0 Shares' }</p>
             </div>
             <div className="stock__numbers">
                 <p className="stock__price">${price}</p>
-                <p className="stock__percentage">+86%</p>
+                <p className={`stock__percentage ${Math.sign(percentage) < 0 ? 'red' : ''}`}>  {Number(percentage).toFixed(2)}%</p>
             </div>
         </div>
     )
