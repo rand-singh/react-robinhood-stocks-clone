617366235571fab9dd214f5a32d4856e179ef401
Randeep Singh <rand-singh@users.noreply.github.com>
2021-02-10 20:48:20 +0000

    Install firebase, fetch data from firebase for our own stocks

--- src/components/Stocks.js
+++ src/components/Stocks.js
@@ -2,11 +2,38 @@ import React, { useState, useEffect } from 'react'
 import { API_KEY_TOKEN } from '../config.json'
 import axios from 'axios'
 import StocksRow from './StocksRow'
+import { db } from '../firebase'
 
 const BASE_URL = "https://finnhub.io/api/v1/quote"
 
 function Stocks() {
     const [stockData, setStockData] = useState([])
+    const [myStocks, setMyStocks] = useState([])
+
+    const getMyStocks = () => {
+        db.collection('myStocks')
+        .onSnapshot(snapshot => {
+            let promises = []
+            let tempData = []
+
+            snapshot.docs.map((doc) => {
+                promises.push(getStockData(doc.data().ticker)
+                .then(res => {
+                    tempData.push({
+                        id: doc.id,
+                        data: doc.data(),
+                        info: res.data
+                    })
+                }))
+            })
+
+            Promise.all(promises).then(() => {
+                console.log(tempData)
+                setMyStocks(tempData)
+            })
+        })
+    }
+
 
     const getStockData = (stock) => {
         return axios
@@ -21,6 +48,7 @@ function Stocks() {
         const stocksList = ["AAPL", "MSFT", "TSLA", "FB", "BABA", "UBER", "DIS", "SBUX"]
 
         let promises = [];
+        getMyStocks();
         stocksList.map((stock) => {
             promises.push(
                 getStockData(stock)
@@ -44,12 +72,13 @@ function Stocks() {
                 <p>Stocks</p>
             </div>
             <div className="stocks__list">
-                {stockData.map((stock) => (
+                {myStocks.map((stock) => (
                     <StocksRow
-                        key={stock.name}
-                        name={stock.name}
-                        openPrice={stock.o}
-                        price={stock.c}
+                        key={stock.data.ticker}
+                        name={stock.data.ticker}
+                        openPrice={stock.info.o}
+                        shares={stock.data.shares}
+                        price={stock.info.c}
                     />
                 ))}
             </div>
@@ -58,7 +87,14 @@ function Stocks() {
                 <p>Lists</p>
             </div>
             <div className="stocks__list">
-                {/* stocks to buy */}
+                {stockData.map((stock) => (
+                    <StocksRow
+                        key={stock.name}
+                        name={stock.name}
+                        openPrice={stock.o}
+                        price={stock.c}
+                    />
+                ))}
             </div>
         </div>
     )
--- src/components/StocksRow.js
+++ src/components/StocksRow.js
@@ -1,16 +1,16 @@
 import React from 'react'
 
-function StocksRow({ name, openPrice, price, shares }) {
+function StocksRow({ name, openPrice, shares, price }) {
     const percentage = ((price - openPrice) / openPrice) * 100
 
     return (
         <div className="stock">
             <div className="stock__intro">
                 <p className="stock__name">{name}</p>
-                <p className="stock__count">{shares ? (shares + " Shares") : '0 Shares' }</p>
+                <p className="stock__count">{shares ? (shares + " Shares") : '' }</p>
             </div>
             <div className="stock__numbers">
-                <p className="stock__price">${price}</p>
+                <p className="stock__price">${price.toFixed(2)}</p>
                 <p className={`stock__percentage ${Math.sign(percentage) < 0 ? 'red' : ''}`}>  {Number(percentage).toFixed(2)}%</p>
             </div>
         </div>
