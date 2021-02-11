553f1cfe52bc464c03246e937bcf0bc20722c45e
Randeep Singh <rand-singh@users.noreply.github.com>
2021-02-11 21:05:12 +0000

    Buy stocks event listener
    Also limit news to 20 articles

--- src/components/MarketNews.js
+++ src/components/MarketNews.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react'
 import axios from 'axios'
 import { API_KEY_TOKEN } from '../config.json'
+import { Carousel } from 'react-responsive-carousel';
 
 const BASE_URL = "https://finnhub.io/api/v1/news?category=general"
 
@@ -22,34 +23,37 @@ function MarketNews() {
         promises.push(getNews()
         .then((res) => {
             res.data.map(article => {
-                tempNews.push({
-                    ...article
-                })
+                if (tempNews.length < 20) {
+                    tempNews.push({
+                        ...article
+                    })
+                }
             })
-        }))        
+
+        }))
 
         Promise.all(promises).then(() => {
             setNews(tempNews)
         })
     }, [])
 
-    console.log(news.length)
-
     return (
         <div className="marketnews__container">
             <h2>Latest Market News</h2>
-            {news.map(item => (
-                <div key={item.id} className="article__container">
-                    <h4>{item.headline}</h4>
-                    <img src={item.image} alt="" width="100"/>
-                    <p>{item.summary}</p>
-                    <a href={item.url}>Read More</a>
-                    <br/>
-                    <small>Source: {item.source}</small>
-                    <br/>
-                    <time>{new Date(1612991492 * 1000).toUTCString()}</time>
-                </div>
-            ))}
+            <Carousel>
+                {news.map(item => (
+                    <div key={item.id} className="article__container">
+                        <h4>{item.headline}</h4>
+                        <p>{item.summary}</p>
+                        <a href={item.url}>Read More</a>
+                        <br/>
+                        <small>Source: {item.source}</small>
+                        <br/>
+                        <img src={item.image} alt=""/>
+                        <time>{new Date(item.datetime * 1000).toUTCString()}</time>
+                    </div>
+                ))}
+            </Carousel>
         </div>
     )
 }
--- src/components/StocksRow.js
+++ src/components/StocksRow.js
@@ -1,10 +1,39 @@
 import React from 'react'
+import { db } from '../firebase'
 
 function StocksRow({ name, openPrice, shares, price }) {
     const percentage = ((price - openPrice) / openPrice) * 100
 
+    const buyStock = () => {
+        db.collection('myStocks')
+        .where("ticker", "==", name)
+        .get()
+        .then((querySnapshot) => {
+            if (!querySnapshot.empty) {
+                // update stock record
+                querySnapshot.forEach((doc) => {
+                    console.log(doc.id, " => ", doc.data())
+                    
+                    db.collection('myStocks')
+                    .doc(doc.id)
+                    .update({
+                        shares: doc.data().shares += 1                        
+                    })
+                })
+            } else {
+                console.log('you dont own this stock yet')
+                db.collection('myStocks')
+                .add({
+                    ticker: name,
+                    shares: 1
+                })
+            }
+
+        })
+    }
+
     return (
-        <div className="stock">
+        <div className="stock" onClick={buyStock}>
             <div className="stock__intro">
                 <p className="stock__name">{name}</p>
                 <p className="stock__count">{shares ? (shares + " Shares") : '' }</p>
